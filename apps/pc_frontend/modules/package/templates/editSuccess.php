6af4c518e461387049aebfdaee877b11b9a7ceda
77web <info@77-web.com>
2010-08-16 21:59:28 -0700

    add delete form

--- apps/pc_frontend/modules/package/templates/editSuccess.php
+++ apps/pc_frontend/modules/package/templates/editSuccess.php
@@ -3,3 +3,7 @@
   'title'       => __('Edit Package'),
   'isMultipart' => true,
 )) ?>
+
+<?php if($isDeletable): ?>
+  <?php op_include_form('PackageDeleteForm', $deleteForm, array('url'=>url_for('package_delete', $package), 'title'=>__('Delete Package'))); ?>
+<?php endif; ?>
